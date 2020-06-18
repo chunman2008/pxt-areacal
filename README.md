320d441ae4df8f44a4f8fa359b488675b4477006
chunman2008 <66762518+chunman2008@users.noreply.github.com>
2020-06-18 17:15:39 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# pxt-areacal
+# tools
+
+areacal11
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+```package
+areacal=github:chunman2008/pxt-areacal
+```
---
+++ main.ts
@@ -0,0 +1,19 @@
+//% weight=0 color=#3CB371 icon="\uf0ad" block="areacal"
+namespace areacal {
+    /**
+    * 
+    */
+    //% blockId="areaOfRectangle" block="area of rectangle length %length|width %width"
+    //% blockGap=2 weight=0 blockExternalInputs=true
+    export function areaOfRectangle(length: number, width:number): number {
+        return length*width
+    }
+    /**
+    * 
+    */
+    //% blockId="ledOfRectangle" block="show area of rectangle length %length|width %width"
+    //% blockGap=2 weight=1
+    export function ledOfRectangle(length: number, width:number): void {
+        basic.showNumber(length*width)
+    }
+}
