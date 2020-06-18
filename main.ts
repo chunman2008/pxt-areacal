1c1b6b3b3d3a24806ed15351cba08d9113c5203d
chunman2008 <66762518+chunman2008@users.noreply.github.com>
2020-06-18 17:30:48 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ namespace areacal {
     * 
     */
     //% blockId="areaOfRectangle" block="area of rectangle length %length|width %width"
-    //% blockGap=2 weight=0 blockExternalInputs=true
+    //% blockGap=3 weight=0 blockExternalInputs=true
     export function areaOfRectangle(length: number, width:number): number {
         return length*width
     }
@@ -12,12 +12,12 @@ namespace areacal {
     * 
     */
     //% blockId="ledOfRectangle" block="show area of rectangle length %length|width %width"
-    //% blockGap=2 weight=1
+    //% blockGap=3 weight=1
     export function ledOfRectangle(length: number, width:number): void {
         basic.showNumber(length*width)
     }
 	//% blockId="ledOfTriangle" block="show area of Triangles base %base|width %base"
-    //% blockGap=2 weight=2
+    //% blockGap=3 weight=2
     export function ledOfTriangle(base: number, height:number): void {
         basic.showNumber(0.5*base*height)
     }
